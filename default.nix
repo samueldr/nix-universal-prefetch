3778aa871dab608c567a9a1610444dc7eceefae5
Samuel Dionne-Riel <samuel@dionne-riel.com>
2019-01-05 00:40:11 -0500

    Changes build

--- default.nix
+++ default.nix
@@ -2,13 +2,13 @@
 
 let
   inherit (pkgs) runCommand ruby;
-  version = "0.0.1";
+  version = "0.1.0";
 in
 # The main script uses nix-shell, for quick iteration or one-off use without building.
 # I find it simpler to just prefix the shebang I want.
 runCommand "nix-universal-prefetch-${version}" {} ''
   mkdir -pv $out/bin
-  echo "#!${ruby}/bin/ruby" > $out/bin/nix-universal-prefetch
-  cat ${./nix-universal-prefetch} >> $out/bin/nix-universal-prefetch
-  chmod +x $out/bin/nix-universal-prefetch
+  cp ${./nix-universal-prefetch} $out/bin/nix-universal-prefetch
+  substituteInPlace "$out/bin/nix-universal-prefetch" \
+    --replace "/usr/bin/env nix-shell" "${ruby}/bin/ruby"
 ''
