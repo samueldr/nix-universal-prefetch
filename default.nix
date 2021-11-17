2c1c674b2164432207508f22629e1a14b193a3d6
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-11-16 19:41:20 -0500

    v0.4.0

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 
 let
   inherit (pkgs) runCommand ruby;
-  version = "0.1.0";
+  version = "0.4.0";
 in
 # The main script uses nix-shell, for quick iteration or one-off use without building.
 # I find it simpler to just prefix the shebang I want.
